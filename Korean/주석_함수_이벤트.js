b6d40278547227a64ee2923d5e6be5264eae5aff
thdtjsdn <thdtjsdn@gmail.com>
2016-09-06 12:52:50 +0900

    [ 파일생성 ] 한글 버전
    [ 파일생성 ] 한글 버전

---
+++ Korean/주석_함수_이벤트.js
@@ -0,0 +1,59 @@
+//----------------------------------------------------------------------------------------------------;
+
+//	Event Listener - Keyboard;
+
+//----------------------------------------------------------------------------------------------------;
+
+/**
+ * @function
+ * @param {Keyboard} e event
+ */
+var evt_kDown__ElementID( e ){};
+
+/**
+ * @function
+ * @param {Keyboard} e event
+ */
+var evt_kUp__ElementID( e ){};
+
+//----------------------------------------------------------------------------------------------------;
+
+//	Event Listener - Mouse;
+
+//----------------------------------------------------------------------------------------------------;
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mClick__ElementID( e ){};
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mDown__ElementID( e ){};
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mMove__ElementID( e ){};
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mOver__ElementID( e ){};
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mOut__ElementID( e ){};
+
+/**
+ * @function
+ * @param {MouseEvent} e event
+ */
+var evt_mUp__ElementID( e ){};
